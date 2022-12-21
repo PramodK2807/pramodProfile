b41be04437cd0805b4b6252b78925b9c62cc6af0
PramodK2808 <pramodkushwaha1328@gmail.com>
2022-12-21 12:23:41 +0530

    code

---
+++ javascript.js
@@ -0,0 +1,50 @@
+
+let header = document.querySelector("header");
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+
+window.addEventListener("scroll", () => {
+    header.classList.toggle("shadow",window.scrollY > 0)
+
+})
+
+
+menu.onclick = () => {
+    navbar.classList.toggle("navbar");
+    navbar.classList.toggle("active");
+}
+
+window.onscroll = () => {
+    navbar.classList.remove("active");
+}
+
+// menu.addEventListener("click" , () => {
+//     navbar.classList.toggle(".active");
+// });
+
+
+menu.onclick = () => {
+    navbar.classList.toggle(".active");
+};
+
+const darkMode = document.querySelector(".darkmode"); 
+
+function changeMode(){
+    let myBody = document.body;
+    myBody.classList.toggle("dark-bg");
+
+    if(darkMode.classList.contains("bx-moon")){
+        darkMode.classList.replace("bx-moon", "bx-sun");
+        darkMode.classList.add( "bx-sun");
+    }
+
+    else{
+        darkMode.classList.replace( "bx-sun", "bx-moon");
+        darkMode.classList.add( "bx-moon");
+    }
+}
+
+
+
+
